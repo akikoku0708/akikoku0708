210f7089c2744ce8e7c3b2e2dfa4841ec7abb412
hotaru <can@applyex.com>
2022-01-21 18:43:43 +0900

    コミットメッセージ2

---
+++ laraproject/app/Http/Controllers/AkiController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AkiController extends Controller
+{
+
+public function hello() {return view('hello');    }
+
+
+
+  
+}
---
+++ laraproject/resources/views/hello.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <title>My First Page</title>
+</head>
+<body>
+
+
+
+
+
+<?php    /*
+  @foreach ($db1 as $rent)
+         <p>
+         {{ $rent->id }}
+         {{ $rent->first_name }}
+         {{ $rent->last_name }}
+         </p>
+     @endforeach
+*/  ?>
+  <h2>111111111111111111Hello World from blade111111111!!55555555555</h2>
+  <p>{{date("Y/m/d H:i:s")}}</p>
+
+
+<p><a href="hello2.blade.php?asd=897"><button>testtest</button></a>xxxx</p>
+
+</body>
+</html>
